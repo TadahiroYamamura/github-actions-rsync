b34b7cfcea93381b3f67c313b639494404e14d61
TadahiroYamamura <mt.village12@gmail.com>
2021-08-04 17:37:30 +0900

    write ssh options in .ssh/config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,13 @@ mkdir -p "$SSH_PATH"
 echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
+# Create SSH config file
+touch "$SSH_PATH/config"
+echo "Host rsync_target"                   >> "$SSH_PATH/config"
+echo "  HostName $SSH_HOSTNAME"            >> "$SSH_PATH/config"
+echo "  User $SSH_USERNAME"                >> "$SSH_PATH/config"
+echo "  IdentityFile $SSH_PATH/deploy_key" >> "$SSH_PATH/config"
+echo "  StrictHostKeyChecking no"          >> "$SSH_PATH/config"
+
 # Do deployment
-sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${GITHUB_WORKSPACE}${INPUT_RSYNC_SOURCE} ${SSH_USERNAME}@${SSH_HOSTNAME}:${INPUT_RSYNC_TARGET}"
+sh -c "rsync ${INPUT_RSYNC_OPTIONS} ${GITHUB_WORKSPACE}${INPUT_RSYNC_SOURCE} rsync_target:${INPUT_RSYNC_TARGET}"
